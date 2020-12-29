539a1ba1e07d753b1890d41166a706256c17628a
Khnykin Andrey <beyondmyspace@yandex.ru>
2020-12-29 09:50:01 +0300

    fixed index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,27 @@
-let formElement = document.querySelector('.popup')
+/* объявляем переменные */
+
+let formElement = document.querySelector('form')
+let popup = document.querySelector('.popup')
 let popupOpenButton = document.querySelector('.info__edit-btn')
 let popupCloseButton = document.querySelector('.popup__close')
-let popupSaveButton = document.querySelector('.popup__save-btn')
 let defaultName = document.querySelector('.info__title')
 let defaultJob = document.querySelector('.info__subtitle')
 let nameInput = document.querySelector('.popup__name')
 let jobInput = document.querySelector('.popup__job')
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
+/* объявляем общую функцию*/
 
-    defaultName.textContent = nameInput.value;
-    defaultJob.textContent = jobInput.value;
+function formSubmitHandler (evt) {
+    popup.classList.toggle('popup_is-opened') /* добавляем toggle в попап, в результате чего класс popup_is-opened добавляется при нажатии на "редактировать" и удаляется при нажатии на крестик. */ 
+    nameInput.value = defaultName.textContent /* получаем информацию из содержимого полей формы */
+    jobInput.value = defaultJob.textContent
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-formElement.addEventListener('click', function() {
-    if (event.target === event.currentTarget) {
-        formElement.classList.remove('popup_is-opened')
-    }
-})
-
-popupOpenButton.addEventListener('click', function(evt) {
-    formElement.classList.add('popup_is-opened');
-
-    nameInput.value = defaultName.textContent
-    jobInput.value = defaultJob.textContent
-    
-})
-
-popupSaveButton.addEventListener('click', function(evt) {
-    nameInput.textContent = defaultName.value
-    formElement.classList.remove('popup_is-opened');
-})
-
-
-popupCloseButton.addEventListener('click', function(evt) {
-    formElement.classList.remove('popup_is-opened');
+popupOpenButton.addEventListener('click', formSubmitHandler) /* делаем кнопку работоспособной при помощи метода addEventListener, события "клик" и ранее объявленной функции */
+popupCloseButton.addEventListener('click', formSubmitHandler)
+formElement.addEventListener('submit', function(evt) {
+    evt.preventDefault(); /* сбрасываем стандартное поведение страницы, отменяем стандартную отправку */
+    defaultName.textContent = nameInput.value /* перезаписываем полученные значения полей, для возможности изменения */
+    defaultJob.textContent = jobInput.value
+    popup.classList.remove('popup_is-opened') /* удаляем класс popup_is-opened при условии использования кнопки перезаписи */
 })
